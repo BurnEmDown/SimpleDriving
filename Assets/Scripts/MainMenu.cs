b3a688ff3ae440fa34ad8a9ba075753dfd3d8b66
BurnEmDown <amitreuveni@gmail.com>
2023-02-01 02:37:12 +0200

    Improved energy mechanics

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,10 +2,12 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
+    [SerializeField] private Button playButton;
     
     [SerializeField] private TMP_Text highScoreText;
     [SerializeField] private TMP_Text energyText;
@@ -19,6 +21,15 @@ public class MainMenu : MonoBehaviour
 
     public void Start()
     {
+        OnApplicationFocus(true);
+    }
+
+    public void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus) return;
+        
+        CancelInvoke();
+        
         int highscore = PlayerPrefs.GetInt(ScoreSystem.HighScoreKey, 0);
 
         highScoreText.text = "High Score: " + highscore.ToString();
@@ -27,6 +38,7 @@ public class MainMenu : MonoBehaviour
 
         if (energy == 0)
         {
+            playButton.interactable = false;
             string energyReadyString = PlayerPrefs.GetString(EnergyReadyKey, string.Empty);
             
             if(energyReadyString == String.Empty) { return; }
@@ -38,8 +50,26 @@ public class MainMenu : MonoBehaviour
                 energy = maxEnergy;
                 PlayerPrefs.SetInt(EnergyKey, energy);
             }
+            else
+            {
+                Debug.Log("Invoking energy recharge in " + (energyReady - DateTime.Now).Seconds + " seconds");
+                Invoke(nameof(EnergyRecharge),(energyReady - DateTime.Now).Seconds);
+            }
+        }
+        else
+        {
+            playButton.interactable = true;
         }
 
+        energyText.text = "Play " + energy;
+    }
+
+    private void EnergyRecharge()
+    {
+        Debug.Log("energy recharge");
+        playButton.interactable = true;
+        energy = maxEnergy;
+        PlayerPrefs.SetInt(EnergyKey, energy);
         energyText.text = "Play " + energy;
     }
 
@@ -52,8 +82,11 @@ public class MainMenu : MonoBehaviour
         PlayerPrefs.SetInt(EnergyKey, energy);
         Debug.Log("energy is " + energy);
         if (energy == 0)
+        {
+            playButton.interactable = false;
             SetRechargeDate();
-        
+        }
+
         SceneManager.LoadScene("Game");
         //energyText.text = "Play " + energy;
     }
